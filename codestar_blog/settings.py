5a6430d243c84c20a24cffbdc79a35b0d3118dba
Richardv10 <uponthemountain@gmail.com>
2025-10-10 16:42:26 +0100

    added key to env

--- codestar_blog/settings.py
+++ codestar_blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#)uwb7=o+bv*$ap=#%%jo0mj*a=*#p#-66=jrb((fk^wmro0ym'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- env.py
+++ env.py
@@ -1,3 +1,5 @@
 import os 
 
 os.environ.setdefault( "DATABASE_URL", "postgresql://neondb_owner:npg_d9NHTlrKLwA7@ep-bitter-grass-aghd8vby.c-2.eu-central-1.aws.neon.tech/yeast_disk_emit_465694")
+
+os.environ.setdefault("SECRET_KEY", "WANGOO69")
