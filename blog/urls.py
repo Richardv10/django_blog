6aef9ec013a6319db877205f5f0dbaa478fbdbb8
Richardv10 <uponthemountain@gmail.com>
2025-10-12 23:28:37 +0100

    added templete folder

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_hello, name='blog_hello'),
+    path('', views.PostList.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views import generic
+from .models import Post
 # Create your views here.
-def blog_hello(request):
-    return HttpResponse("Hello from the blog!")
+
+class PostList(generic.ListView):
+    model = Post
