244f35134d99402e9e58726018aad11297f9123f
Richardv10 <uponthemountain@gmail.com>
2025-10-18 15:43:16 +0100

    when will this end??

--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name ='comments')
     author = models.ForeignKey(User, on_delete = models.CASCADE, related_name ='commenter')
-    body = models.TextField()
+    body = models.TextField(null=True)
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add =True)
     class Meta:
